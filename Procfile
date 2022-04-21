79c698aacc32ccf3bdb2e6daef490d9ba50a136f
satpreet234 <singhsatpreet13@gmail.com>
2022-04-21 10:57:21 +0530

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web:node taxiBooker/app.js
--- connection/connection.js
+++ connection/connection.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 const dotenv=require('dotenv');
 var connect=()=>{
-    const url=process.env.MONGO_PORT;
+    const url='mongodb+srv://root:root@cluster0.todu8.mongodb.net/clients?retryWrites=true&w=majority';
     return mongoose.connect(url,{useNewUrlParser:true},(err,data)=>{
         if(err){throw err}
         else{return console.log('database connected succesfully')}
